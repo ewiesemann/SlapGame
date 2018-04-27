9f10401ebee9f80fc0e793bc880fae35a35a72d7
Name <username@email.com>
2018-04-27 15:52:12 -0600

    Updated button funtion

--- game.js
+++ game.js
@@ -1,20 +1,33 @@
+var name = "Death-Star"
 var health = 100
+var hits = 0
 
 function slap(){
     health -= 1;
-    alert (health);
+    hits += 1;
+    //alert(health);
+    update();
 }
 
 function slap2(){
-    health -= 5;
-    alert (health);
+    health -= 5;s
+    hits += 1;
+    update();
 }
 
 function slap3(){
     health -= 10;
-    alert (health);
+    hits += 1;
+    update();
+}
+
+function update(){
+
+    document.getElementById("health").innerText=`${health}`;
+    document.getElementById("name").innerText=`${name}`;
+    document.getElementById("hits").innerText=`${hits}`;
+    
 }
 
 
-
-
+update();
