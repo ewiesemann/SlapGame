5105f5cce53cb263eeb4de51cdd460d0727871cf
Name <username@email.com>
2018-04-27 14:40:36 -0600

    Added button fucntions

---
+++ game.js
@@ -0,0 +1,20 @@
+var health = 100
+
+function slap(){
+    health -= 1;
+    alert (health);
+}
+
+function slap2(){
+    health -= 5;
+    alert (health);
+}
+
+function slap3(){
+    health -= 10;
+    alert (health);
+}
+
+
+
+
