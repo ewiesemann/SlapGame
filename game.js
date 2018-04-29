556ef8b63582e3d3761ca18c94ce1f34c5df2834
Name <username@email.com>
2018-04-28 21:12:46 -0600

    Updated constructor functions

--- game.js
+++ game.js
@@ -2,25 +2,97 @@
 //var health = 100
 //var hits = 0
 
-var xwing = new Player("XWing")
+var xwing = new Player("Xwing")
 
-function slap(){
-    health -= 1;
-    hits += 1;
+function player(Xwing){
+    this.Xwing = Xwing;
+    this.health = 100;
+    this.hits = 0;
+};
+
+function player2(Deathstar){
+    this.Deathstar = Deathstar;
+    this.health = 100;
+    this.hits = 1;
+};
+
+function Attack(dmg){
+    this.dmg = dmg
+}
+
+var attack = function(laser, canon, photon){
+    this.laser = laser;
+    this.canon = canon;
+    this.photon = photon;
+}
+
+function attack(laser){
+    Deathstar.health -= 1;
+    Deathstar.hits +1
+    update()
+}
+
+function attack(canon){
+    Deathstar.health -= 5;
+    Deathstar.hits +1
+    update()
+}
+
+function attack(photon){
+    Deathstar.health -= 10;
+    Deathstar.hits +1
+    update()
+}
+
+var items = {
+    laser: new item("Laser"),
+    canon: new item ("Canon"),
+    photon: new item ("Photon"),
+    tiefighter: new item ("Tie Fighter"),
+    stardestroyer:new item ("Star Destroyer"),
+    shield: new item ("Shield"),
+}
+
+function defense(avoid){
+    this.avoid = avoid
+}
+
+var defense = function(tiefighter, stardestroyer, shield){
+    this.tiefighter = tiefighter;
+    this.stardestroyer = stardestroyer;
+    this.shield = shield;
+}
+
+
+
+function defense(tiefighter){
+    Deathstar.health += 1;
+    Deathstar.hits += 1;
     update();
 }
 
-function slap2(){
-    health -= 5;
-    hits += 1;
+
+
+function defense(stardestroyer){
+    Deathstar.health += 2;
+    Deathstar.hits += 2;
     update();
 }
 
-function slap3(){
-    health -= 10;
-    hits += 1;
+
+
+function defense(shield){
+    Deathstar.health += 3;
+    Deathstar.hits += 3;
     update();
 }
+
+
+
+var laser = new Attack(-1)
+var canon = new Attack(-5)
+var Photon = new Attack(-10)
+
 
 function update(){
 
@@ -30,31 +102,33 @@ function update(){
     
 }
 
-function Player(name){
-    this.name = name
-    this.health = 100
-    this.hits = 0
-}
 
-function Attack(dmg){
-    this.dmg = dmg
-}
-
-var laser = new Attack(-1)
-var canon = new Attack(-5)
-var Photon = new Attack(-10)
-
-function Defense(dfs){
-    this.dfs = dfs
-}
-
-var dive = new Defense(1)
-var roll = new Defense (5)
-var shield = new Defense (10)
 
 
 
 update();
+
+
+
+
+//function slap(){
+//    health -= 1;
+//    hits += 1;
+//    update();
+//}
+
+//function slap2(){
+//    health -= 5;
+//    hits += 1;
+//    update();
+//}
+
+//function slap3(){
+//    health -= 10;
+//    hits += 1;
+//    update();
+//}
+
 
 // var me = {
 //    modifiers:[{name: 'modify1'}]
