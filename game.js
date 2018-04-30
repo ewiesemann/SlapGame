c0027a8ec8384173be549d9257402f4dbdae3a8c
Name <username@email.com>
2018-04-30 09:10:13 -0600

    Trying to resolve issues with attack buttons

--- game.js
+++ game.js
@@ -97,8 +97,6 @@ function dmgReduce() {
     enemy.equip = [];
 }
 
-
-
 update();
 
 
@@ -110,79 +108,4 @@ update();
 
 
 
-    //var name = "Death-Star"
-    //var health = 100
-    //var hits = 0
-
-    //var xwing = new player("Xwing")
-
-    //function player(Xwing){
-        //    this.Xwing = Xwing;
-        //    this.health = 100;
-        //    this.hits = 0;
-        //};
-    /** 
-     function attack(laser){
-         Deathstar.health -= 1;
-         Deathstar.hits +1
-         update()
-        }
-        
-        function attack(canon){
-    Deathstar.health -= 5;
-    Deathstar.hits +1
-    update()
-}
-
-function attack(photon){
-    Deathstar.health -= 10;
-    Deathstar.hits +1
-    update()
-}
-*/
-
-
-//var laser = new Attack(-1)
-//var canon = new Attack(-5)
-//var Photon = new Attack(-10)
-
-
-
-
-
-
-
-
-// var me = {
-//    modifiers:[{name: 'modify1'}]
-// }
-
-
-// var modifiers = {
-//    mod1: {name: "modifiers1}"};
-//   }
-
-
-
-// function modify1(){
-//    me.modifiers.push(modifiers.mod1)  
-//        draw()
-// }
-
-
-//  function addMods(){
-//    for (let i = 0; i < me.modifiers.length; index++) {
-//       let mod = me.modifiers[i];
-//      total+= mod.name + ","       
-//    }    
-//  return total
-//  }
-
-//  function draw(){
-//    var results = addMods()
-//    document.getElementById('modifiers').innerText = results
-//  }
-
-// var slap1(){
-//    me.health -=5 + addMods()    
-//  }
+    
